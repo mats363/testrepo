c790d1b806fb58f4cc73b23e76def4660af586e8
mats363 <81287846+mats363@users.noreply.github.com>
2021-11-16 23:04:02 +0100

    adds js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hejsan hörru');
+
+const message = `Hej`;
+
+
+let textBox = document.getElementById("textBox");
+//textBox.insertAdjacentHTML("afterbegin", message);
+
+let fName = prompt("Hej vad heter du?");
+let lName = prompt("I efternamn då?");
+let userName = fName + "" + lName;
+
+textBox.insertAdjacentHTML("afterbegin", "<div class=''>" + userName + "</div>" );
